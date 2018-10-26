f13d7fad22eb2aadbc2a45ca9c1bef206fee418f
Pradip Khadka <fnfdrifter@gmail.com>
2018-10-26 22:13:51 +0545

    Firebase config

--- src/admin/firebase_config/config.js
+++ src/admin/firebase_config/config.js
@@ -1,10 +1,10 @@
 const config = {
-  apiKey: 'AIzaSyDN6vYIk-89PI1y1gmI8P-33hvZ8NxT3iI',
-  authDomain: 'tamiatcms.firebaseapp.com',
-  databaseURL: 'https://tamiatcms.firebaseio.com',
-  projectId: 'tamiatcms',
-  storageBucket: 'tamiatcms.appspot.com',
-  messagingSenderId: '94963392700'
+  apiKey: "AIzaSyDuMiXZjVy18yq7by3g4w9AwVlcnb9qn5E",
+  authDomain: "tamiat-org.firebaseapp.com",
+  databaseURL: "https://tamiat-org.firebaseio.com",
+  projectId: "tamiat-org",
+  storageBucket: "tamiat-org.appspot.com",
+  messagingSenderId: "922627318234"
 }
 
 export default config
